aae2f26f4718c993f26b0938d59d8fd3ac9c04e6
pradeep2421 <pradeeppatidar1999@gmail.com>
2022-08-31 23:30:01 +0530

    feat: connected mysql with go

--- backend/main.go
+++ backend/main.go
@@ -1,15 +1,31 @@
 package main
 
 import (
+	"backend/config"
+	"backend/models"
 	Routers "backend/routers"
 	"backend/utils"
 
+	_ "github.com/go-sql-driver/mysql"
+
+	"github.com/jinzhu/gorm"
 	"go.uber.org/zap"
 )
-func main(){
+func init(){ 
 	utils.InitializeLogger()
-	utils.SugarLogger.Info("Hlllllweeer")
-	utils.SugarLogger.Error("Not able to log.",zap.String("url", "codewithmukesh.com"))
+
+}
+var err error
+func main(){
+	config.DB, err = gorm.Open("mysql", config.DbURL(config.BuildDBConfig()))
+	if err != nil {
+		utils.SugarLogger.Error("DataBase is not connected",zap.Error(err))
+	}
+ err = config.DB.AutoMigrate(&models.User{}).Error
+ if err != nil {
+	utils.SugarLogger.Error("DataBase is not connected",zap.Error(err))
+	return
+}
 	r := Routers.SetupRouter()
 	
 	r.Run()
